18b34f4e226557358be31d0b101409a2eafdfac8
asadbek <ibnnumon@gmail.com>
2023-03-06 15:34:01 +0500

    fixed

--- models/productSchema.js
+++ models/productSchema.js
@@ -2,22 +2,9 @@ const mongoose = require('mongoose')
 
 const productSchema = mongoose.Schema(
   {
-    name: {
+    title: {
       type: String,
       required: [true, 'pls enter product name']
-    },
-    quantity: {
-      type: Number,
-      required: true,
-      default: 0
-    },
-    price: {
-      type: Number,
-      required: true
-    },
-    image: {
-      type: String,
-      required: false
     }
   },
   {
